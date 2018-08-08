607ccfa13133e52fae9c3ad05b54fc23456db77f
glancer000 <contactgregtucker@gmail.com>
2018-08-08 16:59:37 -0300

    added eslint prettier

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from "react";
 
-const LandingPage = () =>
+const LandingPage = () => (
   <div>
     <h1>Landing</h1>
     <p>The Landing Page.</p>
   </div>
+);
 
 export default LandingPage;
